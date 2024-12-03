c37c421c841973acdcbe43bc294af6039564b496
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-12-03 20:03:44 +0200

    💸 Add filtering options to ShoppingList component; implement price sorting functionality

--- client/src/scenes/home/ShoppingList.jsx
+++ client/src/scenes/home/ShoppingList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Box, Typography, Tab, Tabs, useMediaQuery } from "@mui/material";
+import { Box, Typography, Tab, Tabs, useMediaQuery, Select, MenuItem } from "@mui/material";
 import Item from "../../components/Item";
 import { setItems } from "../../state";
 import axios from "axios";
@@ -10,11 +10,16 @@ const URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:1337";
 const ShoppingList = () => {
   const dispatch = useDispatch();
   const [value, setValue] = useState("all");
+  const [filter, setFilter] = useState("default");
   const items = useSelector((state) => state.cart.items);
   const isNonMobile = useMediaQuery("(min-width:600px)");
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
+  };
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
   };
 
   const getItems = async () => {
@@ -40,6 +45,16 @@ const ShoppingList = () => {
     (item) => item.attributes.category === "bestSellers"
   );
 
+  const sortItems = (items) => {
+    const itemsCopy = [...items];
+    if (filter === "priceLowToHigh") {
+      return itemsCopy.sort((a, b) => a.attributes.price - b.attributes.price);
+    } else if (filter === "priceHighToLow") {
+      return itemsCopy.sort((a, b) => b.attributes.price - a.attributes.price);
+    }
+    return itemsCopy;
+  };
+
   return (
     <Box width="80%" margin="80px auto">
       <Typography variant="h3" textAlign="center">
@@ -64,6 +79,13 @@ const ShoppingList = () => {
         <Tab label="BEST SELLERS" value="bestSellers" />
         <Tab label="TOP RATED" value="topRated" />
       </Tabs>
+      <Box display="flex" justifyContent="flex-end" mb={2}>
+        <Select value={filter} onChange={handleFilterChange}>
+          <MenuItem value="default">Default</MenuItem>
+          <MenuItem value="priceLowToHigh">Price: Low to High</MenuItem>
+          <MenuItem value="priceHighToLow">Price: High to Low</MenuItem>
+        </Select>
+      </Box>
       <Box
         margin="0 auto"
         display="grid"
@@ -73,19 +95,19 @@ const ShoppingList = () => {
         columnGap="1.33%"
       >
         {value === "all" &&
-          items.map((item) => (
+          sortItems(items).map((item) => (
             <Item item={item} key={`${item.name}-${item.id}`} />
           ))}
         {value === "newArrivals" &&
-          newArrivalsItems.map((item) => (
+          sortItems(newArrivalsItems).map((item) => (
             <Item item={item} key={`${item.name}-${item.id}`} />
           ))}
         {value === "bestSellers" &&
-          bestSellersItems.map((item) => (
+          sortItems(bestSellersItems).map((item) => (
             <Item item={item} key={`${item.name}-${item.id}`} />
           ))}
         {value === "topRated" &&
-          topRatedItems.map((item) => (
+          sortItems(topRatedItems).map((item) => (
             <Item item={item} key={`${item.name}-${item.id}`} />
           ))}
       </Box>
