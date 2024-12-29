a958fec397ec89fa3b9b2ea1647db5460b79b5fc
kaloyan51 <kakoyan.ivanov@gmail.com>
2024-12-29 13:12:34 +0200

    📸 Add Instagram libriry with template

--- client/src/scenes/home/WelcomePage.jsx
+++ client/src/scenes/home/WelcomePage.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react';
 
 const WelcomePage = () => {
+  const elfsightRef = useRef(null);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://static.elfsight.com/platform/platform.js';
+    script.async = true;
+
+    if (elfsightRef.current) {
+      elfsightRef.current.appendChild(script);
+    }
+
+    return () => {
+      if (elfsightRef.current) {
+        elfsightRef.current.innerHTML = '';
+      }
+    };
+  }, []);
+
   return (
     <div>
-        <h1>Welcome to the store</h1>
-        <h2>Instagram gallery expected</h2>
-    </div>
-  )
-}
+      <h1>Welcome to the store</h1>
+      <h2>Instagram Gallery</h2>
 
-export default WelcomePage
+      <div
+        ref={elfsightRef}
+        className="elfsight-app-aab9349d-1ec7-4630-bf00-9134953a4904"
+        data-elfsight-app-lazy
+      ></div>
+    </div>
+  );
+};
+
+export default WelcomePage;
